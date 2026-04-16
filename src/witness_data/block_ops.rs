3cc5854640b43846e834bf1b9f7ffd9fb55b6746
Kelsey Merrill <kelseymerrill18@gmail.com>
2026-04-16 13:56:59 -0400

    remove stale TODO comment

--- src/witness_data/block_ops.rs
+++ src/witness_data/block_ops.rs
@@ -544,7 +544,6 @@ mod tests {
 
     #[test]
     fn test_compute_lookups_zero_blocks() {
-        // TODO: add test that has a zero block_chunk checks for a zero decomp block
         let row: &[bool] = &[
             false, false, false, false, false, true, true, true, true, true, true, true, true,
             true, true, true, true, true, true, true, true, true, true, false, false, false, false,
